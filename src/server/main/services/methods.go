b8ebb1006fcae1a096d1b6c479fb0b7d1855bad4
Jacopo Fabi <jacopo.fabi1997@gmail.com>
2021-09-17 18:52:47 +0200

    modify service

--- src/server/main/services/methods.go
+++ src/server/main/services/methods.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ListenHttpConnection() {
-	e := http.ListenAndServe(":1234", nil)
+	e := http.ListenAndServe(":80", nil)
 	if e != nil {
 		log.Fatal("Listen error: ", e)
 	}
@@ -23,26 +23,26 @@ type Args struct {
 type ServizioDiProva int
 
 // Metodo 1 dell'interfaccia
-func (s *ServizioDiProva) Stampa1(args *Args, reply *int) error{
+func (s *ServizioDiProva) Stampa1(args *Args, reply *int) error {
 	fmt.Printf("Stampa1")
 	return nil
 }
 
 // Metodo 2 dell'interfaccia
-func (s *ServizioDiProva) Stampa2(args *Args, reply *int) error{
+func (s *ServizioDiProva) Stampa2(args *Args, reply *int) error {
 	fmt.Printf("Stampa2")
 	return nil
 }
 
 // Metodo 3 dell'interfaccia che lista tutti i Metodi Remoti disponibili
-func (s *ServizioDiProva) ListMethods(args *Args, reply *string) error{
-	*reply = 	"============= METHODS LIST =============\n"+
-				"1) Metodo1\n"+
-				"2) Metodo2\n"
+func (s *ServizioDiProva) ListMethods(args *Args, reply *string) error {
+	*reply = "============= METHODS LIST =============\n" +
+		"1) Metodo1\n" +
+		"2) Metodo2\n"
 	return nil
 }
 
-func InitializeService() *ServizioDiProva{
-	service := new (ServizioDiProva)
+func InitializeService() *ServizioDiProva {
+	service := new(ServizioDiProva)
 	return service
 }
