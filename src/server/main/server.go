2e4088d867722ec283a03c7ff339e0f7b72e8a6e
danilo-dellorco <danilodellorcolp@gmail.com>
2021-09-17 17:49:21 +0200

    Inserito Handle HTTP

--- src/server/main/server.go
+++ src/server/main/server.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"../main/services"
 	"net/rpc"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
 func main() {
+	go ListenHealthPing()
 	if len(os.Args)!=1{
 		fmt.Printf("Usage: go run server.go\n")
 	}
@@ -17,4 +21,26 @@ func main() {
 	rpc.Register(service)
 	rpc.HandleHTTP()
 	services.ListenHttpConnection()
+}
+
+
+
+func ListenHealthPing() {
+	fmt.Printf("Starting Helth Service\n")
+	handler := http.HandlerFunc(handleRequest)
+	http.Handle("/example", handler)
+	http.ListenAndServe(":80", nil)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header()."Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["message"] = "Status OK"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+	return
 }
