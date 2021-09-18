092c2c919978434ab110dc98bae3116696e93af8
danilo-dellorco <danilodellorcolp@gmail.com>
2021-09-18 16:19:11 +0200

    modificato script

--- script.py
+++ script.py
@@ -2,7 +2,8 @@ import os;
 os.system("sudo cp /home/ec2-user/progetto-sdcc/src/server/progettosdcc.service /etc/systemd/system")
 os.system("sudo systemctl daemon-reload")
 os.system("sudo systemctl start progettosdcc.service")
+os.system("sudo systemctl enable progettosdcc.service")
+
 
 # Stampa lo stato del servizio e delle porte
-os.system("sudo systemctl status progettosdcc.service -l")
-os.system("sudo lsof -i -P -n")
+os.system("sudo systemctl status progettosdcc.service -l")  
