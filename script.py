f27a0faede05cb302ceb8129c8e3058cabe00cff
danilo-dellorco <danilodellorcolp@gmail.com>
2021-09-18 13:19:53 +0200

    add service status

--- script.py
+++ script.py
@@ -2,3 +2,4 @@ import os;
 os.system("sudo cp /home/ec2-user/progetto-sdcc/src/server/progettosdcc.service /etc/systemd/system")
 os.system("sudo systemctl daemon-reload")
 os.system("sudo systemctl start progettosdcc.service")
+os.system("sudo systemctl status progettosdcc.service -l")
