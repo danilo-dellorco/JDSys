efbef6ba040ccb31d99f00caaec094808a09a99d
danilo-dellorco <danilodellorcolp@gmail.com>
2021-09-22 17:35:07 +0200

    quasi terminato parsing risposte

--- registry/main/registry.go
+++ registry/main/registry.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"progetto-sdcc/utils"
-	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/aws/aws-sdk-go/service/elbv2"
 )
 
@@ -19,9 +17,12 @@ var TEST_INSTANCE string = "i-0a7f1097d88fd8d43"
 //TODO marshal del JSON errore primo parametro di unmarshal
 func main() {
 	targetGroup := getTargetGroup(ELB_ARN)
-	fmt.Println(targetGroup)
-	//getInstancesFromGroup(targetGroup)
-	//getTargetsHealth()
+	targetGroupArn := getTargetGroupArn(targetGroup)
+	targetsHealth := getTargetsHealth(targetGroupArn)
+	healthyInstancesList := getHealthyInstancesId(targetsHealth)
+	for _, id := range healthyInstancesList {
+		fmt.Println(getInstanceInfo(id))
+	}
 	//getInstanceInfo("i-0a7f1097d88fd8d43")
 	//targetGroupJson := getTargetGroup()
 	//var targetGroup TargetGroup
@@ -32,7 +33,9 @@ func main() {
 func createSession() *session.Session {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1")})
-	fmt.Println(err)
+	if err != nil {
+		fmt.Println(err)
+	}
 	return sess
 }
 
@@ -62,11 +65,11 @@ func getInstanceInfo(instanceId string) string {
 	return "nil"
 }
 
-func getTargetsHealth() {
+func getTargetsHealth(targetGroupArn string) *elbv2.DescribeTargetHealthOutput {
 	sess := createSession()
 	svc := elbv2.New(sess)
 	input := &elbv2.DescribeTargetHealthInput{
-		TargetGroupArn: aws.String("arn:aws:elasticloadbalancing:us-east-1:806961903927:targetgroup/progetto-sdcc-target-group/4c8603e9d9c32e53"),
+		TargetGroupArn: aws.String(targetGroupArn),
 	}
 
 	result, err := svc.DescribeTargetHealth(input)
@@ -87,13 +90,11 @@ func getTargetsHealth() {
 			// Message from an error.
 			fmt.Println(err.Error())
 		}
-		return
 	}
-
-	fmt.Println(result)
+	return result
 }
 
-func getTargetGroup(elbArn string) string {
+func getTargetGroup(elbArn string) *elbv2.DescribeTargetGroupsOutput {
 	sess := createSession()
 	svc := elbv2.New(sess)
 	input := &elbv2.DescribeTargetGroupsInput{
@@ -116,50 +117,42 @@ func getTargetGroup(elbArn string) string {
 			// Message from an error.
 			fmt.Println(err.Error())
 		}
-		return result.GoString()
 	}
-	return "nil"
-}
-
-func ExampleELB_DescribeInstanceHealth_shared00() {
-	sess := createSession()
-
-	svc := elb.New(sess)
-	input := &elb.DescribeInstanceHealthInput{
-		LoadBalancerName: aws.String("NetworkLB"),
-	}
-
-	result, err := svc.DescribeInstanceHealth(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case elb.ErrCodeAccessPointNotFoundException:
-				fmt.Println(elb.ErrCodeAccessPointNotFoundException, aerr.Error())
-			case elb.ErrCodeInvalidEndPointException:
-				fmt.Println(elb.ErrCodeInvalidEndPointException, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return
-	}
-
-	fmt.Println(result)
+	return result
 }
 
 func getInstancePublicIp(instanceInfo string) string {
-	return utils.GetStringInBetween(instanceInfo, "PublicIpAddress: ", ",")
+	return utils.GetStringInBetween(instanceInfo, "PublicIpAddress: \"", "\",")
 }
 
 func getInstancePrivateIp(instanceInfo string) string {
-	return utils.GetStringInBetween(instanceInfo, "PublicIpAddress: ", ",")
+	return utils.GetStringInBetween(instanceInfo, "PrivateIpAddress: \"", "\",")
 }
 
-func getInstancesFromGroup(groupResult string) {
-	split := strings.Split(groupResult, "},{")
-	fmt.Println(split[0])
+func getTargetGroupArn(targetGroupResult *elbv2.DescribeTargetGroupsOutput) string {
+	return utils.GetStringInBetween(targetGroupResult.String(), "TargetGroupArn: \"", "\",")
+}
+
+/*
+func getInstancesFromGroup(groupResult *elbv2.DescribeTargetGroupsOutput) {
+	var list = strings.Fields(groupResult.TargetGroups[0].String())
+}
+*/
+
+func getHealthyInstancesId(targetHealth *elbv2.DescribeTargetHealthOutput) []string {
+	//retrieve private and public IP addresses associated to every ec2 instance
+	var healthyNodes []string
+	descriptions := targetHealth.TargetHealthDescriptions
+	for i := 0; i < len(descriptions); i++ {
+		actual := descriptions[i].String()
+		fmt.Println(actual)
+		id := utils.GetStringInBetween(actual, "Id: \"", "\",")
+		state := utils.GetStringInBetween(actual, "State: \"", "\"")
+		fmt.Println(id)
+		fmt.Println(state)
+		if state == "healthy" {
+			healthyNodes = append(healthyNodes, id)
+		}
+	}
+	return healthyNodes
 }
