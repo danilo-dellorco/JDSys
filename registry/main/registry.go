af04a1075d3b6e9170ecf073af2ae95fd5b0b77c
Jacopo Fabi <jacopo.fabi1997@gmail.com>
2021-09-22 19:25:18 +0200

    retrieve IP ec2 instances from lb

--- registry/main/registry.go
+++ registry/main/registry.go
@@ -11,22 +11,34 @@ import (
 	"github.com/aws/aws-sdk-go/service/elbv2"
 )
 
-var ELB_ARN string = "arn:aws:elasticloadbalancing:us-east-1:427788101608:loadbalancer/net/NetworkLB/8d7f674bf6bc6f73"
+var ELB_ARN_D string = "arn:aws:elasticloadbalancing:us-east-1:427788101608:loadbalancer/net/NetworkLB/8d7f674bf6bc6f73"
+var ELB_ARN_J string = "arn:aws:elasticloadbalancing:us-east-1:806961903927:loadbalancer/net/progetto-sdcc-lb/639e06d499fd2aba"
 var TEST_INSTANCE string = "i-0a7f1097d88fd8d43"
+
+type Instance struct {
+	ID, PrivateIP, PublicIP string
+}
 
 //TODO marshal del JSON errore primo parametro di unmarshal
 func main() {
-	targetGroup := getTargetGroup(ELB_ARN)
+	nodes := make(map[int]Instance)
+	targetGroup := getTargetGroup(ELB_ARN_J)
 	targetGroupArn := getTargetGroupArn(targetGroup)
 	targetsHealth := getTargetsHealth(targetGroupArn)
 	healthyInstancesList := getHealthyInstancesId(targetsHealth)
-	for _, id := range healthyInstancesList {
-		fmt.Println(getInstanceInfo(id))
+	fmt.Println("Healthy Instances: ")
+	fmt.Println(healthyInstancesList)
+
+	for i := 0; i < len(healthyInstancesList); i++ {
+		instance := getInstanceInfo(healthyInstancesList[i])
+		nodes[i] = getInstanceAddress(instance)
 	}
-	//getInstanceInfo("i-0a7f1097d88fd8d43")
-	//targetGroupJson := getTargetGroup()
-	//var targetGroup TargetGroup
-	//json.Unmarshal(targetGroupJson, &targetGroup)
+
+	fmt.Println("Address Healthy Instances: ")
+	for key, element := range nodes {
+		fmt.Println("Key: ", key, "=>", "Element:", element)
+	}
+
 	return
 }
 
@@ -39,19 +51,21 @@ func createSession() *session.Session {
 	return sess
 }
 
-func getInstanceInfo(instanceId string) string {
+func getTargetGroup(elbArn string) *elbv2.DescribeTargetGroupsOutput {
 	sess := createSession()
-	svc := ec2.New(sess)
-	input := &ec2.DescribeInstancesInput{
-		InstanceIds: []*string{
-			aws.String(instanceId),
-		},
+	svc := elbv2.New(sess)
+	input := &elbv2.DescribeTargetGroupsInput{
+		LoadBalancerArn: aws.String(elbArn),
 	}
 
-	result, err := svc.DescribeInstances(input)
+	result, err := svc.DescribeTargetGroups(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
+			case elbv2.ErrCodeLoadBalancerNotFoundException:
+				fmt.Println(elbv2.ErrCodeLoadBalancerNotFoundException, aerr.Error())
+			case elbv2.ErrCodeTargetGroupNotFoundException:
+				fmt.Println(elbv2.ErrCodeTargetGroupNotFoundException, aerr.Error())
 			default:
 				fmt.Println(aerr.Error())
 			}
@@ -60,9 +74,8 @@ func getInstanceInfo(instanceId string) string {
 			// Message from an error.
 			fmt.Println(err.Error())
 		}
-		return result.GoString()
 	}
-	return "nil"
+	return result
 }
 
 func getTargetsHealth(targetGroupArn string) *elbv2.DescribeTargetHealthOutput {
@@ -94,21 +107,37 @@ func getTargetsHealth(targetGroupArn string) *elbv2.DescribeTargetHealthOutput {
 	return result
 }
 
-func getTargetGroup(elbArn string) *elbv2.DescribeTargetGroupsOutput {
+func getHealthyInstancesId(targetHealth *elbv2.DescribeTargetHealthOutput) []string {
+	//retrieve the ID associated to every healthy ec2 instance
+	var healthyNodes []string
+	descriptions := targetHealth.TargetHealthDescriptions
+	for i := 0; i < len(descriptions); i++ {
+		actual := descriptions[i].String()
+		//fmt.Println(actual)
+		id := utils.GetStringInBetween(actual, "Id: \"", "\",")
+		state := utils.GetStringInBetween(actual, "State: \"", "\"")
+		//fmt.Println(id)
+		//fmt.Println(state)
+		if state == "healthy" {
+			healthyNodes = append(healthyNodes, id)
+		}
+	}
+	return healthyNodes
+}
+
+func getInstanceInfo(instanceId string) *ec2.DescribeInstancesOutput {
 	sess := createSession()
-	svc := elbv2.New(sess)
-	input := &elbv2.DescribeTargetGroupsInput{
-		LoadBalancerArn: aws.String(elbArn),
+	svc := ec2.New(sess)
+	input := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{
+			aws.String(instanceId),
+		},
 	}
 
-	result, err := svc.DescribeTargetGroups(input)
+	result, err := svc.DescribeInstances(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
-			case elbv2.ErrCodeLoadBalancerNotFoundException:
-				fmt.Println(elbv2.ErrCodeLoadBalancerNotFoundException, aerr.Error())
-			case elbv2.ErrCodeTargetGroupNotFoundException:
-				fmt.Println(elbv2.ErrCodeTargetGroupNotFoundException, aerr.Error())
 			default:
 				fmt.Println(aerr.Error())
 			}
@@ -119,6 +148,20 @@ func getTargetGroup(elbArn string) *elbv2.DescribeTargetGroupsOutput {
 		}
 	}
 	return result
+}
+
+func getInstanceAddress(instanceInfo *ec2.DescribeInstancesOutput) Instance {
+	//retrieve private and public IP addresses associated to an ec2 instance
+	descriptions := instanceInfo.Reservations
+	actual := descriptions[0].String()
+	//fmt.Println(actual)
+	id := utils.GetStringInBetween(actual, "InstanceId: \"", "\",")
+	public := utils.GetStringInBetween(actual, "PublicIpAddress: \"", "\"")
+	private := utils.GetStringInBetween(actual, "PrivateIpAddress: \"", "\"")
+	//fmt.Println(id)
+	//fmt.Println(public)
+	//fmt.Println(private)
+	return Instance{id, public, private}
 }
 
 func getInstancePublicIp(instanceInfo string) string {
@@ -138,21 +181,3 @@ func getInstancesFromGroup(groupResult *elbv2.DescribeTargetGroupsOutput) {
 	var list = strings.Fields(groupResult.TargetGroups[0].String())
 }
 */
-
-func getHealthyInstancesId(targetHealth *elbv2.DescribeTargetHealthOutput) []string {
-	//retrieve private and public IP addresses associated to every ec2 instance
-	var healthyNodes []string
-	descriptions := targetHealth.TargetHealthDescriptions
-	for i := 0; i < len(descriptions); i++ {
-		actual := descriptions[i].String()
-		fmt.Println(actual)
-		id := utils.GetStringInBetween(actual, "Id: \"", "\",")
-		state := utils.GetStringInBetween(actual, "State: \"", "\"")
-		fmt.Println(id)
-		fmt.Println(state)
-		if state == "healthy" {
-			healthyNodes = append(healthyNodes, id)
-		}
-	}
-	return healthyNodes
-}
