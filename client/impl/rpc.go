2b1a0ae45588e7b50eb790fdf13c0300347c4afa
jacopofabi <jacopo.fabi1997@gmail.com>
2021-11-15 10:34:50 +0100

    rpc retransmit

--- client/impl/rpc.go
+++ client/impl/rpc.go
@@ -3,6 +3,9 @@ package impl
 import (
 	"fmt"
 	"log"
+	"net/rpc"
+	"progetto-sdcc/utils"
+	"time"
 )
 
 func GetRPC(key string) {
@@ -11,11 +14,10 @@ func GetRPC(key string) {
 
 	var reply *string
 
+	c := make(chan string)
+
 	client, _ := HttpConnect()
-	err := client.Call("RPCservice.GetRPC", args, &reply)
-	if err != nil {
-		log.Fatal("RPC error: ", err)
-	}
+	CallRPC(client, args, *reply, c)
 	fmt.Println("Risposta RPC:", *reply)
 }
 
@@ -59,4 +61,30 @@ func DeleteRPC(key string) {
 		log.Fatal("RPC error: ", err)
 	}
 	fmt.Println("Risposta RPC:", *reply)
+}
+
+func rr1_timeout(client *rpc.Client, args Args1, reply string, c chan string) {
+	for {
+		time.Sleep(utils.RR1_TIMEOUT)
+		fmt.Println("scaduto timer")
+		reply := <-c
+		fmt.Println(reply)
+		if reply == "" {
+			CallRPC(client, args, reply, c)
+		} else {
+			break
+		}
+	}
+}
+
+func CallRPC(client *rpc.Client, args Args1, reply string, c chan string) {
+	go rr1_timeout(client, args, reply, c)
+	fmt.Println("prima call")
+	err := client.Call("RPCservice.GetRPC", args, &reply)
+	fmt.Println("dopo call")
+	fmt.Println(reply)
+	if err != nil {
+		log.Fatal("RPC error: ", err)
+	}
+	c <- reply
 }
