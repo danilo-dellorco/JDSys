1c6ad10fb166518d84be83a280f9b848991e26a5
jacopofabi <jacopo.fabi1997@gmail.com>
2021-11-20 18:20:11 +0100

    prova

--- node/impl/rpc.go
+++ node/impl/rpc.go
@@ -157,7 +157,7 @@ func (n *Node) PutImpl(args Args, reply *string) error {
 	}
 	utils.PrintTs(*reply)
 	utils.PrintTs("Finished. Replying to caller")
-
+	fmt.Println("che cazzo è:", ok)
 	// inserimento avvenuto correttamente, procediamo con l'invio della replica al successore
 	if ok {
 		fmt.Println("diocane")
