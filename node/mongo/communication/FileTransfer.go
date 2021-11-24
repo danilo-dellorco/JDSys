f5c15c20bee6d7164eff70922f9dab96ed8f4861
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2021-11-24 12:30:28 +0100

    Start Changing FileTransfer folder

--- node/mongo/communication/FileTransfer.go
+++ node/mongo/communication/FileTransfer.go
@@ -50,7 +50,8 @@ func StartSender(filename string, address string, mode string) error {
 	default:
 		addr = address + utils.FILETR_RECONCILIATION_PORT
 	}
-	connection, err := net.DialTimeout("tcp", addr, 1*time.Second)
+	// TODO dimensionare t/o o toglierlo
+	connection, err := net.DialTimeout("tcp", addr, 20*time.Second)
 	if err != nil {
 		utils.PrintTs(err.Error())
 		return err
